d8ab39f2f023996a4296e1f37c58fff0be82b498
William Vita <williamvita@hotmail.com>
2023-06-03 11:28:00 -0400

    blog list added

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+# Create your models here. Toda tabla viene con una llave primaria (Primary Key)
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     author = models.ForeignKey(
         "auth.User", 
-        on_delete=models.CASCADE, 
+        on_delete=models.CASCADE, # on delete: si se elimina el author, se borra todo lo queéste incluyó
     )
-    def __str__(self):
-        return self.title
+    def __str__(self): # devuelve el nombre del post que colocamos
+          return self.title
     
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={"pk": self.pk}) # entre llaves = diccionario python /post_detal ya que la app se llama post
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'home.html'
